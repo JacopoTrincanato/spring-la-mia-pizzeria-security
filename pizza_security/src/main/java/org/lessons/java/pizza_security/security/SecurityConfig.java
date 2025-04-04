a31459cdf51180683a5b7c191a1797c0f103dd03
JacopoTrincanato <trincanatoj@gmail.com>
2025-04-04 17:12:07 +0200

    create a method for regulate filters

---
+++ pizza_security/src/main/java/org/lessons/java/pizza_security/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package org.lessons.java.pizza_security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // creo un metodo per la filterChain
+    @Bean
+    @SuppressWarnings("removal")
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // regole per i filtri
+        httpSecurity.authorizeHttpRequests().requestMatchers("/pizze/create", "/pizze/edit/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/pizze/**").hasAuthority("ADMIN")
+                .requestMatchers("/offerte_speciali", "/offerte_speciali/**").hasAuthority("ADMIN")
+                .requestMatchers("/ingredienti", "/ingredienti/**").hasAuthority("ADMIN")
+                .requestMatchers("/pizze", "/pizze/**").hasAnyAuthority("USER", "ADMIN").requestMatchers("/**")
+                .permitAll().and().formLogin().and().logout().and().exceptionHandling();
+    }
+}
